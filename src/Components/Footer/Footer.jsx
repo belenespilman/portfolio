ccb64e148db34f6a201955e3eccccd8720c5148e
belenespilman <belen.espilman@gmail.com>
2024-01-26 15:38:23 +0100

    Fix footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import "./Footer.css";
 import Wave from "../../img/wave.png";
-import Insta from "@iconscout/react-unicons/icons/uil-instagram";
-import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
-import Gitub from "@iconscout/react-unicons/icons/uil-github";
+import Github from "@iconscout/react-unicons/icons/uil-github";
+import Linkedin from "@iconscout/react-unicons/icons/uil-linkedin"
 
 const Footer = () => {
   return (
     <div className="footer">
-      <img src={Wave} alt="" style={{ width: "100%" }} />
+      <img src={Wave} alt="" className="wave" />
       <div className="f-content">
-        <span>Zainkeepscode@gmail.com</span>
+        <span>belenespilman@gmail.com</span>
         <div className="f-icons">
-          <Insta color="white" size={"3rem"} />
-          <Facebook color="white" size={"3rem"} />
-          <Gitub color="white" size={"3rem"} />
+          <a href="https://www.linkedin.com/in/belen-espilman/" target={"_blank"}>
+            <Linkedin color="white" size={"3rem"} />
+          </a>
+          <a href="https://github.com/belenespilman" target={"_blank"}>
+            <Github color="white" size={"3rem"} />
+          </a>
         </div>
       </div>
     </div>
